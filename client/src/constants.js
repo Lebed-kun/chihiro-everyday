cd271ff3f5a6272a82e764ba05bda15728139910
Lebed-kun <lebed-kun@mail.ru>
2019-05-21 12:58:46 +0400

    Fix style

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,5 @@
 export const BASE_URL = 'http://localhost:8000';
+//export const BASE_URL = 'https://chiblog.herokuapp.com';
 export const PREVIEW_MAX_LENGTH = 1000; // chars count
 export const COMMENT_MAX_LENGTH = 1500; // chars count
 export const COMMENT_POST_INTERVAL = 7000; // milliseconds
